e4a9963ae43aa01131d30a17f757f78f915c6388
andrefelix.br@gmail.com <andrefelix.br>
2014-04-04 01:55:17 -0300

    Alteração da classe AloMundo

--- AloMundo/src/br/com/feltex/alomundo/AloMundo.java
+++ AloMundo/src/br/com/feltex/alomundo/AloMundo.java
@@ -1,8 +1,11 @@
 package br.com.feltex.alomundo;
 
+import java.util.Date;
+
 public class AloMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Alo Mundo");
+		System.out.println("Data de Hoje: " + new Date());
 	}
 }
